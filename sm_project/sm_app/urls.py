3ffd38263a267589cafe7dcb5822887c5af6fbf5
Lilys-Code <lilybwatt@outlook.com>
2023-02-09 13:41:14 +1100

    Added basic checklist screen view

--- sm_project/sm_app/urls.py
+++ sm_project/sm_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'sm_app'
 urlpatterns = [
-    
+    path('<int:checklist_id>/', views.checklist_view, name='checklist'),
 ]
--- sm_project/sm_app/views.py
+++ sm_project/sm_app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import User, Checklist, ChecklistItem
+
+def checklist_view(request, checklist_id):
+    checklist = get_object_or_404(Checklist, pk=checklist_id)
+    return render(request, 'sm_app/checklist.html', {'checklist': checklist})

bd27d9aafec96088e4c13004b886342b85d4d872
Lilys-Code <lilybwatt@outlook.com>
2023-03-31 14:07:02 +1100

    Added unit tests for project models

--- sm_project/cl_app/tests.py
+++ sm_project/cl_app/tests.py
@@ -1,3 +1,67 @@
 from django.test import TestCase
 
-# Create your tests here.
+from sm_project.cl_app.models import Checklist, ChecklistItem
+from sm_project.user_app.models import User
+
+
+class ChecklistModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user1 = User.objects.create_user("user1@test.com", "Test User 1", "test1")
+        cls.user2 = User.objects.create_user("user2@test.com", "Test User 2", "test2")
+        cls.test1 = Checklist.objects.create(checklist_title="Checklist Test 1", creator=cls.user1)
+        cls.test2 = Checklist.objects.create(checklist_title="Checklist Test 2", creator=cls.user2)
+        cls.test1.researchers.add(cls.user1)
+        cls.test1.reviewers.add(cls.user2)
+        cls.test2.researchers.add(cls.user2)
+        cls.test2.reviewers.add(cls.user1)
+    
+    def test__str__(self):
+        self.assertEqual(self.test1.__str__(), "Checklist Test 1")
+        self.assertEqual(self.test2.__str__(), "Checklist Test 2")
+    
+    def test_creator_name(self):
+        self.assertEqual(self.test1.creator_name, "Test User 1")
+        self.assertEqual(self.test2.creator_name, "Test User 2")
+    
+    def test_researcher_emails(self):
+        self.assertEqual(self.test1.researcher_emails(), ["user1@test.com"])
+        self.assertEqual(self.test2.researcher_emails(), ["user2@test.com"])
+    
+    def test_reviewer_emails(self):
+        self.assertEqual(self.test1.reviewer_emails(), ["user2@test.com"])
+        self.assertEqual(self.test2.reviewer_emails(), ["user1@test.com"])
+
+class ChecklistItemModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user1 = User.objects.create_user("user1@test.com", "Test User 1", "test1")
+        cls.test1 = Checklist.objects.create(checklist_title="Checklist Test 1", creator=cls.user1)
+        cls.test2 = Checklist.objects.create(checklist_title="Checklist Test 2", creator=cls.user1)
+        cls.item1_1 = ChecklistItem.objects.create(item_checklist=cls.test1, item_title="Item 1 - Checklist 1", item_status=2)
+        cls.item2_1 = ChecklistItem.objects.create(item_checklist=cls.test1, item_title="Item 2 - Checklist 1", item_status=3)
+        cls.item1_2 = ChecklistItem.objects.create(item_checklist=cls.test2, item_title="Item 1 - Checklist 2", item_status=1)
+        cls.item2_2 = ChecklistItem.objects.create(item_checklist=cls.test2, item_title="Item 2 - Checklist 2", item_status=3)
+        cls.item2_1.dependencies.add(cls.item1_1)
+        cls.item2_1.dependencies.add(cls.item2_1)
+        cls.item2_2.dependencies.add(cls.item1_2)
+    
+    def test__str__(self):
+        self.assertEqual(self.item1_1.__str__(), "Item 1 - Checklist 1")
+        self.assertEqual(self.item2_1.__str__(), "Item 2 - Checklist 1")
+    
+    def test_get_status(self):
+        self.assertEqual(self.item1_1.get_status(), "For Review")
+        self.assertEqual(self.item2_1.get_status(), "Incomplete")
+        self.assertEqual(self.item1_2.get_status(), "Completed")
+
+    def test_get_dependancies(self):
+        self.assertEqual(self.item1_1.get_dependencies(), "")
+        self.assertEqual(self.item2_1.get_dependencies(), " - Dependant On: Item 1 - Checklist 1, Item 2 - Checklist 1")
+        self.assertEqual(self.item2_2.get_dependencies(), " - Dependant On: Item 1 - Checklist 2")
+    
+    def test_dependencies_completed(self):
+        self.assertTrue(self.item1_1.dependencies_completed())
+        self.assertFalse(self.item2_1.dependencies_completed())
+        self.assertTrue(self.item2_2.dependencies_completed())
+
--- sm_project/user_app/tests.py
+++ sm_project/user_app/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
 
-# Create your tests here.
+from sm_project.cl_app.models import Checklist
+from sm_project.user_app.models import User
+
+
+class UserModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user1 = User.objects.create_superuser("user1@test.com", "Test User 1", "test1")
+        cls.user2 = User.objects.create_user("user2@test.com", "Test User 2", "test2")
+        cls.test1 = Checklist.objects.create(checklist_title=f"Temp{cls.user1.id}", creator=cls.user1)
+    
+    def test__str__(self):
+        self.assertEqual(self.user1.__str__(), "Test User 1 - user1@test.com")
+        self.assertEqual(self.user2.__str__(), "Test User 2 - user2@test.com")
+
+    def test_is_staff(self):
+        self.assertTrue(self.user1.is_staff)
+
+    def test_is_admin(self):
+        self.assertTrue(self.user1.is_admin)
+
+    def test_has_temp_checklist(self):
+        self.assertTrue(self.user1.has_temp_checklist())
+        self.assertFalse(self.user2.has_temp_checklist())
+
+    def test_get_temp_checklist(self):
+        self.assertEqual(self.user1.get_temp_checklist(), self.test1)
+

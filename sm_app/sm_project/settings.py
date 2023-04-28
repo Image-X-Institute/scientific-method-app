c2d8321e0b28f358a663942b5edb1cd4f223e081
Lilys-Code <lilybwatt@outlook.com>
2023-04-28 12:37:53 +1000

    Updated ALLOWED_HOSTS in settings.py

--- sm_app/sm_project/settings.py
+++ sm_app/sm_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '').split(" ")
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost:1337"]
 
